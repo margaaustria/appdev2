7e5b5d1348da5c33ba986d0b28e71853c12cf1da
margaaustria <calumpianomargarette@gmail.com>
2026-02-17 23:22:55 +0800

    Fixing App.js file

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,20 +1,27 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState('Login');
+
+  const navigationProps = {
+    navigate: (screen) => setCurrentScreen(screen),
+    goBack: () => setCurrentScreen('Login'),
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      {currentScreen === 'Login' ? (
+        <Login navigation={navigationProps} />
+      ) : (
+        <Signup navigation={navigationProps} />
+      )}
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
