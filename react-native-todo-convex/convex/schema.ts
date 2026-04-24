c5077c11bf27551cd73d3fe04e4a478543b8b585
margaaustria <calumpianomargarette@gmail.com>
2026-04-24 14:01:29 +0800

    Done with Create DB Schema

--- react-native-todo-convex/convex/schema.ts
+++ react-native-todo-convex/convex/schema.ts
@@ -4,6 +4,7 @@ import { v } from "convex/values";
 export default defineSchema({
   todos: defineTable({
     text: v.string(),
-    completed: v.boolean(),
-  }),
+    isCompleted: v.boolean(),
+  })
 });
+
